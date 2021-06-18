6d026d4bf55118838ad552c8654656513a225607
Sandyabhi <abhisandy47@gmail.com>
2021-06-18 13:00:34 +0530

    cc

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+  uri: "https://polar-reef-02771.herokuapp.com/s",
 });
 
 const authLink = setContext(() => {
